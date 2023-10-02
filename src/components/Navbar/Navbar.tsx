23da1fd2470d331ec2ee091bf9684ef677c0484b
UT LogicLabs <jadix117@gmail.com>
2023-10-02 12:10:51 -0600

    Rename NavbarTwoColumns * Renamed NavbarTwoColumns.types to Navbar.types * Updated imports for Navbar and renamed default export * Updated .npmignore * Updated package.json version

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import { I_Navbar } from "./NavbarTwoColumns.types";
+import { I_Navbar } from "./Navbar.types";
 
-export default function NavbarTwoColumns({ children }: I_Navbar) {
+export default function Navbar({ children }: I_Navbar) {
   return (
     <div className="flex flex-col gap-y-3 sm:flex-row sm:items-center sm:justify-between">
       {children}
